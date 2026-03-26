0df1f99897f6498f89358230dc8dfd278ec12a4a
MBD <divinemountso@gmail.com>
2026-03-26 23:42:06 +0300

    "Hand Tracking module"

--- HandTracking.py
+++ HandTracking.py
@@ -8,7 +8,8 @@ mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
 
-pTime = time.time()  # Initialize previous time outside the loop
+pTime = 0
+cTime = 0
 
 while True:
     success, img = cap.read()
@@ -20,13 +21,21 @@ while True:
     # print(results.multi_hand_landmarks)
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                print(id,lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                print(id,cx,cy)
+
+                cv2.circle(img,(cx,cy),15,(255,0,255),cv2.FILLED)
+
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
     cTime = time.time()
     fps = 1 / (cTime - pTime) if (cTime - pTime) != 0 else 0
     pTime = cTime  # Update previous time after calculating FPS
 
-    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 255), 3)
 
     cv2.imshow('Image', img)
     cv2.waitKey(1)
