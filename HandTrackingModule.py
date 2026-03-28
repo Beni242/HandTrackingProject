c5bb01707649e71f9bbd9f097739907c9e69eda9
MBD <divinemountso@gmail.com>
2026-03-28 21:48:37 +0300

    class handDetector

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -2,37 +2,42 @@ import cv2
 import mediapipe as mp
 import time
 
-cap = cv2.VideoCapture(0)
+
+class handDectector():
+    def __init__(self):
+
+
 
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
 
+imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+results = hands.process(imgRGB)
+# print(results.multi_hand_landmarks)
+if results.multi_hand_landmarks:
+    for handLms in results.multi_hand_landmarks:
+        for id, lm in enumerate(handLms.landmark):
+            print(id, lm)
+            h, w, c = img.shape
+            cx, cy = int(lm.x * w), int(lm.y * h)
+            print(id, cx, cy)
+
+            cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+
+        mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
 
 def main():
     pTime = 0
     cTime = 0
+    cap = cv2.VideoCapture(0)
 
     while True:
         success, img = cap.read()
         if not success:
             break
 
-        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        results = hands.process(imgRGB)
-        # print(results.multi_hand_landmarks)
-        if results.multi_hand_landmarks:
-            for handLms in results.multi_hand_landmarks:
-                for id, lm in enumerate(handLms.landmark):
-                    print(id, lm)
-                    h, w, c = img.shape
-                    cx, cy = int(lm.x * w), int(lm.y * h)
-                    print(id, cx, cy)
-
-                    cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
-
-                mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
         cTime = time.time()
         fps = 1 / (cTime - pTime) if (cTime - pTime) != 0 else 0
