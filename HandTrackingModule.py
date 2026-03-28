ca3568c6168314f8ad69a3df517166c6d81fc8b7
MBD <divinemountso@gmail.com>
2026-03-28 22:02:04 +0300

    def findposition

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -4,37 +4,52 @@ import time
 
 
 class handDectector():
-    def __init__(self):
+    def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.detectionCon, self.trackCon) #
+        self.mpDraw = mp.solutions.drawing_utils
 
 
+    def findHands(self, img, draw=True):
 
-mpHands = mp.solutions.hands
-hands = mpHands.Hands()
-mpDraw = mp.solutions.drawing_utils
 
-imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-results = hands.process(imgRGB)
-# print(results.multi_hand_landmarks)
-if results.multi_hand_landmarks:
-    for handLms in results.multi_hand_landmarks:
-        for id, lm in enumerate(handLms.landmark):
-            print(id, lm)
-            h, w, c = img.shape
-            cx, cy = int(lm.x * w), int(lm.y * h)
-            print(id, cx, cy)
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+        # print(results.multi_hand_landmarks)
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+        return img
 
-            cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
 
-        mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+    def findPosition(self, img, handNo, draw=True):
+             lmlist = []
+             if self.results.multi_hand_landmarks:
 
+                 for id, lm in enumerate(handLms.landmark):
+                    print(id, lm)
+                    h, w, c = img.shape
+                    cx, cy = int(lm.x * w), int(lm.y * h)
+                    print(id, cx, cy)
+
+                    cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+
+             return lmlist
 
 def main():
     pTime = 0
     cTime = 0
     cap = cv2.VideoCapture(0)
-
+    detector = handDectector()
     while True:
         success, img = cap.read()
+        detector.findHands(img, draw=False)
         if not success:
             break
 
